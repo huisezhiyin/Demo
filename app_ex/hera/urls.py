9d27d6fcac21658ff6c37cb27b0c44dbcc80d88b
Grey <837364695@qq.com>
2018-12-27 20:53:14 +0800

    [build] urls

--- app_ex/hera/urls.py
+++ app_ex/hera/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
+from app_ex.hera import views
 
 router = DefaultRouter()
-#router.register(r'hello', views.HelloWorldViewSet, base_name="hello")
+router.register(r'user', views.UserViewSet, base_name="user")
 urlpatterns = router.urls
